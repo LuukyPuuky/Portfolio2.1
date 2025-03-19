976185f748fe3adb62c88bc533359a4149ef18ea
Luuk Steijaert <537956@student.fontys.nl>
2025-03-19 11:51:17 +0100

    initiate nuxt project portfolio + updated v4 tailwind
    Nog geen readme file toegevoegd maar voor de rest wel alles klaargezet voor portfolio.

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script>
+import Navbar from "./components/Navbar.vue";
+</script>
+<template>
+  <body class="moving-gradient overflow-hidden">
+    <Navbar />
+    <div>
+      <h1
+        class="flex items-center justify-center w-screen h-screen text-white text-4xl"
+      >
+        Vue.js
+      </h1>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+.moving-gradient {
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <nav class="flex items-center justify-between p-8 mx-6">
+      <a href="/" class="text-lg font-medium"> Luuk Steijaert </a>
+      <div class="flex gap-8">
+        <a
+          v-for="(item, index) in navItems"
+          :key="index"
+          :href="item.url"
+          class="text-lg font-medium text-green-500 hover: text-amber-300"
+        >
+          {{ item.label }}
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      navItems: [
+        { label: "Learning Outcomes", url: "/learning-outcomes" },
+        { label: "Projects", url: "/projects" },
+        { label: "About", url: "/about" },
+      ],
+    };
+  },
+};
+</script>
