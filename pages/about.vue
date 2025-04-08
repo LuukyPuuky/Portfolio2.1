662abaf01b36043823be7289309b9006bb6e3bb6
Luuk Steijaert <537956@student.fontys.nl>
2025-04-08 10:15:00 +0200

    updated about.vue init state

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+  <div class="scrollbar scrollbar-thumb-green-500 scrollbar-track-slate-900">
+    <div class="bg-slate-900 text-white">
+      <Navbar />
+      <HeaderProject title="About" />
+    </div>
+  </div>
+</template>
