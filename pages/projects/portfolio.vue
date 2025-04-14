a2d36ed698c92f111f1f2b81344f7a6f1efa3ff6
Luuk Steijaert <537956@student.fontys.nl>
2025-04-14 23:44:42 +0200

    update portfolio, not finished yet but next commit will be something different

--- pages/projects/portfolio.vue
+++ pages/projects/portfolio.vue
@@ -3,9 +3,16 @@
     <div class="bg-slate-900 text-white">
       <Navbar />
       <HeaderProject title="Portfolio" subtitle="Luuk Steijaert" />
-
-      <Teamroles />
-      <Personal />
+      <div class="min-h-screen p-4">
+        <div class="max-w-6xl mx-auto">
+          <div id="teamroles" class="scroll-mt-24">
+            <Teamroles />
+          </div>
+          <div id="personal" class="scroll-mt-24">
+            <Personal />
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   </div>
